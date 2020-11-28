d13a0e96f891b991469b6945c433343932bd515a
Chris K <ikim1991@gmail.com>
2020-11-28 17:22:34 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/styles/App.scss';
+
+const App = () => {
+    return(
+        <div className='App'>
+            
+        </div>
+    )
+};
+
+export default App;
