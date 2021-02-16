2064740ed51f24f1e3181ff152b937a53951ecad
Chris K <ikim1991@gmail.com>
2021-02-16 01:11:14 -0500

    Golf Tracker - Bugs

--- src/components/golf-tracker/content/Bugs.tsx
+++ src/components/golf-tracker/content/Bugs.tsx
@@ -6,13 +6,14 @@ const Bugs = () => {
             <div className="section-item">
                 <h3>Testing</h3>
                 <p>No testing frameworks have been implemented for this application.</p>
-                <p>A functionality test was done by the developer of this application and no reportable bugs have been found.</p>
+                <p>A functionality test was done by the developer of this application.</p>
                 <p>To report any bugs or changes, please feel free to contribute through GitHub.</p>
             </div>
             <div className="section-item">
                 <h3>Errors & Bugs</h3>
                 <p>This application uses try and catch blocks to handle simple errors on the back-end. Any API Errors will result in a Error Status 500.</p>
-                <p>No game breaking bugs have been reported to this date.</p>
+                <p>When a User is not found in the database, an Error Status of 404 will be the result.</p>
+                <p>Currently there is one application-breaking bug found. In the process of logging a new round, the user is prompted a dropdown menu and a date picker, if the user does not specify a dropdown menu selection and proceeds to press the Show Course button, the application crashes as it tries to handle the null value. This will be fixed on the next update.</p>
             </div>
             <div className="section-item">
                 <h3>Features</h3>
