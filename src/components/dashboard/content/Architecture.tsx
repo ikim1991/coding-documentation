f69f5145ad97be1b36887538ab1b36606cc2a72f
Chris K <ikim1991@gmail.com>
2021-02-16 21:27:21 -0500

    Image Files

--- src/components/dashboard/content/Architecture.tsx
+++ src/components/dashboard/content/Architecture.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-
+import stack from './images/stack.png';
+// import pipeline from './images/pipeline.png';
 
 const Architecture = () => {
     return(
         <div className="architecture">
             <div className="section-item">
-                <h3>Application Architecture</h3>
-                <p>The image below illustrates a high level overview of the application's architecture.</p>
-                <img className="item-image"/>
+                <h3>Application Stack</h3>
+                <p>The image below illustrates a high level overview of the application's stack.</p>
+                <img className="item-image" src={stack}/>
             </div>
             <div className="section-item">
                 <h3>Application Pipeline</h3>
                 <p>The image below illustrates a high level overview of the application's pipeline.</p>
-                <img className="item-image"/>
+                {/* <img className="item-image" src={pipeline}/> */}
             </div>
         </div>
     )
